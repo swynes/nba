9e90df68fb5ea2b722e01272571570066898ab51
Seth <jane@example.com>
2019-10-04 21:30:05 -0700

    Updated read.csv so they locate to laptop and desktop

--- nba_data_cleaning.R
+++ nba_data_cleaning.R
@@ -1,9 +1,10 @@
-arena_airports=read.csv("C:/Users/AsusW10/Documents/Aviation/Arena_airports.csv")
+arena_airports=read.csv("C:/Users/AsusW10/Documents/Aviation/NBA/Arena_airports.csv")
 
-nba_schedule=read.csv("C:/Users/AsusW10/Documents/Aviation/NBA_2018_19.csv")
+nba_schedule=read.csv("C:/Users/AsusW10/Documents/Aviation/NBA/NBA_2018_19.csv")
 
 #Import IATA airport codes
 airport_codes <- read.csv("C:/Users/Seth/Documents/airport_codes.csv", header = TRUE)
+airport_codes <- read.csv("C:/Users/AsusW10/Documents/Aviation/NBA/airport_codes.csv", header=T)
 
 library(tidyverse)
 ?left_join

39d7dc6ffa1c3619ec635d084aca46ad10c17e8f
Seth <jane@example.com>
2019-10-04 21:50:24 -0700

    Updated read files to work on laptop as well as desktop

--- Route Map.R
+++ Route Map.R
@@ -1,58 +1,18 @@
-######################  ROUTE MAP  #######################
-
-
-
-getwd()
-
-# Read flight list
-flights <- read.csv(("C:/Users/Seth/Documents/nba/flights.csv"), stringsAsFactors = FALSE)
-
-# Lookup coordinates
+###################  MAPPING THE NBA SEASON    #####################
+library(magrittr)
 library(ggmap)
 library(tidyverse)
 
-air_codes<- read.csv("C:/Users/Seth/Documents/nba/airport_codes.csv")
-View(air_codes)
-
-flights2<- merge(flights, air_codes, by.x="From", by.y="aircode")
-flights2<- merge(flights2, air_codes, by.x="To", by.y="aircode")
-
-View(flights2)
-
-# Plot flight routes
-library(ggrepel)
-library(cowplot)
-library(maps)
-install.packages("cowplot")
-
-worldmap <- borders("world", colour="#efede1", fill="#efede1") # create a layer of borders
-flight_map<- ggplot() + worldmap + 
-  geom_curve(data=flights2, aes(x = long.x, y = lat.x, xend = long.y, yend = lat.y), 
-             col = "#b29e7d", size = 0.3, curvature = .2, ncp=1000, lineend="round") + 
-  #geom_point(data=flights2, aes(x = long.x, y = lat.x), col = "#970027") + 
-  #geom_text_repel(data=air_codes, aes(x = long, y = lat, label = air_codes), col = "black", size = 2, segment.color = NA) + 
-  theme(panel.background = element_rect(fill="white"), 
-        axis.line = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y = element_blank()
-  )
-
-flight_map
-
-save_plot("flight_map.tiff", flight_map, base_aspect_ratio = 1, 
-          dpi=600)
-
-
-
-###################  MAPPING THE NBA SEASON    #####################
-library(magrittr)
-
 nba_schedule<- read.csv(("C:/Users/Seth/Documents/nba/NBA_2018_19.csv"), stringsAsFactors = FALSE)
+nba_schedule<- read.csv(("C:/Users/AsusW10/Documents/Aviation/NBA/NBA_2018_19.csv"), stringsAsFactors = FALSE)
 
 arena_airports<- read.csv("C:/Users/Seth/Documents/nba/Arena_airports.csv")
+arena_airports=read.csv("C:/Users/AsusW10/Documents/Aviation/NBA/Arena_airports.csv")
+
+#Import IATA airport codes
+air_codes <- read.csv("C:/Users/Seth/Documents/airport_codes.csv", header = TRUE)
+air_codes <- read.csv("C:/Users/AsusW10/Documents/Aviation/NBA/airport_codes.csv", header=T)
+
 
 nba_schedule_2 <- right_join(arena_airports,nba_schedule, by="Location")
 
@@ -69,9 +29,9 @@ View(Toronto)
 
 Toronto$origin <- as.character(Toronto$origin)
 Toronto$origin <- as.character(Toronto$destination)
+
 #Replace NA in column with the most frequent value in the column which should be homecourt
 #This ensures a flight home at the end of the season
-
 Toronto$destination[is.na(Toronto$destination)] <- names(which.max(table(Toronto$destination)))
 
 View(Toronto)
@@ -79,12 +39,13 @@ View(Toronto)
 #Remove rows where origin is same as destination (no movement)
 Toronto2<- Toronto[!duplicated(Toronto[,c('origin', 'destination')]),]
 
-Toronto3<- Toronto %>% distinct(origin, destination, .keep_all = TRUE)
-Toronto3<- Toronto3 %>% distinct(origin, destination, .keep_all = TRUE)
+Toronto3<- Toronto2 %>% distinct(origin, destination, .keep_all = TRUE)
+
+setwd("C:/Users/AsusW10/Documents/Aviation/NBA")
 
 write.csv(Toronto3, "Toronto.csv")
 Toronto4<- read.csv("C:/Users/Seth/Documents/nba/Toronto.csv")
-
+Toronto4<- read.csv("C:/Users/AsusW10/Documents/Aviation/NBA/Toronto.csv")
 View(Toronto4)
 
 
@@ -93,6 +54,11 @@ typeof(Toronto$destination)
 
 
 #######################  NOW ADD MAP FOR SEASON   ############################
+
+library(ggrepel)
+library(cowplot)
+library(maps)
+
 Toronto_flights<- merge(Toronto4, air_codes, by.x="origin", by.y="aircode")
 Toronto_flights2<- merge(Toronto_flights, air_codes, by.x="destination", by.y="aircode")
 
@@ -117,5 +83,3 @@ flight_map
 
 save_plot("flight_map.tiff", flight_map, base_aspect_ratio = 1, 
           dpi=600)
-
-
